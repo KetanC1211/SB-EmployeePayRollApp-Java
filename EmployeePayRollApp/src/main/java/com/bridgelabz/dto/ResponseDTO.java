f2fc13eb0feb0153204b4ee0fc8dbb313681c850
KetanC1211 <ketanchindarkar12@gmail.com>
2022-09-20 16:10:42 +0530

    Added log method to log details in application.log

--- EmployeePayRollApp/src/main/java/com/bridgelabz/dto/ResponseDTO.java
+++ EmployeePayRollApp/src/main/java/com/bridgelabz/dto/ResponseDTO.java
@@ -1,20 +1,13 @@
 package com.bridgelabz.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDTO {
     private String message;
     private Object data;
-
-    public ResponseDTO(String message, Object data) {
-        this.message = message;
-        this.data = data;
-    }
-     @Override
-    public String toString() {
-        return "ResponseDTO{" +
-                "message='" + message + '\'' +
-                ", data=" + data +
-                '}';
-    }
 }
