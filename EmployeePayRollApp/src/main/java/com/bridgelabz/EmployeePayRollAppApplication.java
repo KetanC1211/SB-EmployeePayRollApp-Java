10568cffd6e09c39882655f316361c2299d52858
KetanC1211 <ketanchindarkar12@gmail.com>
2022-09-20 17:10:31 +0530

    Added application-dev and application-prod.properties files

--- EmployeePayRollApp/src/main/java/com/bridgelabz/EmployeePayRollAppApplication.java
+++ EmployeePayRollApp/src/main/java/com/bridgelabz/EmployeePayRollAppApplication.java
@@ -3,14 +3,15 @@ package com.bridgelabz;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayRollAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayRollAppApplication.class, args);
-        log.info("Employee Payroll App Started");
+        ApplicationContext context = SpringApplication.run(EmployeePayRollAppApplication.class, args);
+        log.info("Employee Payroll App Started in {} Enivroment",context.getEnvironment().getProperty("enviroment"));
     }
 
 }
